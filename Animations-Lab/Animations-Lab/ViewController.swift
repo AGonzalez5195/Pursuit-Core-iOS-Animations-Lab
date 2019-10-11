7cf0cc5c081da12252f100f6e38e789019ec7bdf
AGonzalez5195 <AnthonyGonzalez@pursuit.org>
2019-10-11 14:34:38 -0400

    added left/right animation movement

--- Animations-Lab/Animations-Lab/ViewController.swift
+++ Animations-Lab/Animations-Lab/ViewController.swift
@@ -1,127 +1,164 @@
+
 //
 //  ViewController.swift
 //  AnimationsPractice
 //
-//  Created by Benjamin Stone on 10/8/19.
-//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//  Created by Anthony Gonzalez on 10/11/19.
+//  Copyright © 2019 Antnee. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: -- Properties
+    lazy var moveUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Move Up", for: .normal)
+        button.titleLabel?.textColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        button.backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.showsTouchWhenHighlighted = true
+        
+        button.addTarget(self, action: #selector(moveUpButtonPressed(sender:)), for: .touchUpInside)
+        return button
+    }()
     
-    lazy var blueSquare: UIView = {
-        let view = UIView()
-        view.backgroundColor = .blue
-        return view
+    lazy var moveDownButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Move Down", for: .normal)
+        button.titleLabel?.textColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        button.backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.showsTouchWhenHighlighted = true
+        
+        button.addTarget(self, action: #selector(moveDownButtonPressed(sender:)), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var moveLeftButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Move Left", for: .normal)
+        button.titleLabel?.textColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        button.backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.showsTouchWhenHighlighted = true
+        
+        button.addTarget(self, action: #selector(moveLeftButtonPressed(sender:)), for: .touchUpInside)
+        return button
+    }()
+    
+    lazy var moveRightButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Move Right", for: .normal)
+        button.titleLabel?.textColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        button.backgroundColor = #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.showsTouchWhenHighlighted = true
+        
+        button.addTarget(self, action: #selector(moveRightButtonPressed(sender:)), for: .touchUpInside)
+        return button
     }()
     
     lazy var buttonStackView: UIStackView = {
-       let buttonStack = UIStackView()
+        let buttonStack = UIStackView(arrangedSubviews: [moveLeftButton,  moveUpButton, moveDownButton, moveRightButton])
         buttonStack.axis = .horizontal
         buttonStack.alignment = .center
-        buttonStack.distribution = .equalSpacing
-        buttonStack.spacing = 30
+        buttonStack.distribution = .fillEqually
+        buttonStack.spacing = 10
         return buttonStack
     }()
     
-    lazy var upButton: UIButton = {
-       let button = UIButton()
-        button.setTitle("Move square up", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .cyan
-        button.addTarget(self, action: #selector(animateSquareUp(sender:)), for: .touchUpInside)
-        return button
+    
+    lazy var myView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        myView.layer.cornerRadius = 50
+        return myView
     }()
     
-    lazy var downButton: UIButton = {
-       let button = UIButton()
-        button.setTitle("Move square down", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .cyan
-        button.addTarget(self, action: #selector(animateSquareDown(sender:)), for: .touchUpInside)
-        return button
+    lazy var animationTimeStepper: UIStepper = {
+        let stepper = UIStepper()
+        
+        return stepper
     }()
     
-    lazy var blueSquareHeightConstaint: NSLayoutConstraint = {
-        blueSquare.heightAnchor.constraint(equalToConstant: 200)
+    //References to prior constraints.
+    lazy var myViewHeightConstraint: NSLayoutConstraint = {
+        myView.heightAnchor.constraint(equalToConstant: 250)
     }()
     
-    lazy var blueSquareWidthConstraint: NSLayoutConstraint = {
-        blueSquare.widthAnchor.constraint(equalToConstant: 200)
+    lazy var myViewWidthConstraint: NSLayoutConstraint = {
+        myView.widthAnchor.constraint(equalToConstant: 250)
     }()
     
-    lazy var blueSquareCenterXConstraint: NSLayoutConstraint = {
-        blueSquare.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+    lazy var myViewCenterXConstraint: NSLayoutConstraint = {
+        myView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
     }()
     
-    lazy var blueSquareCenterYConstraint: NSLayoutConstraint = {
-        blueSquare.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    lazy var myViewCenterYConstraint: NSLayoutConstraint = {
+        myView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        addSubviews()
-        configureConstraints()
-    }
     
-    @IBAction func animateSquareUp(sender: UIButton) {
-        let oldOffset = blueSquareCenterYConstraint.constant
-        blueSquareCenterYConstraint.constant = oldOffset - 150
-        UIView.animate(withDuration: 2) { [unowned self] in
+    //MARK: -- Methods
+    @objc func moveUpButtonPressed(sender: UIButton) {
+        let oldOffset = myViewCenterYConstraint.constant
+        myViewCenterYConstraint.constant = oldOffset - 125 //Takes the prior constraint constant as a reference point and designates a new point.
+        UIView.animate(withDuration: 1.3) {
             self.view.layoutIfNeeded()
         }
     }
     
-    @IBAction func animateSquareDown(sender: UIButton) {
-        let oldOffet = blueSquareCenterYConstraint.constant
-        blueSquareCenterYConstraint.constant = oldOffet + 150
-        UIView.animate(withDuration: 2) { [unowned self] in
+    //        UIView.animate(withDuration: 1.3, delay: 0, options: [.autoreverse,.repeat], animations: {self.view.layoutIfNeeded()}, completion: nil)
+    
+    
+    //'layoutIfNeeded()' animates any constraint changes done for whole view
+    
+    @objc func moveDownButtonPressed(sender: UIButton) {
+        let oldOffet = myViewCenterYConstraint.constant
+        myViewCenterYConstraint.constant = oldOffet + 125
+        UIView.animate(withDuration: 1.3) {
             self.view.layoutIfNeeded()
         }
     }
     
-    private func addSubviews() {
-        view.addSubview(blueSquare)
-        addStackViewSubviews()
-        view.addSubview(buttonStackView)
+    @objc func moveLeftButtonPressed(sender: UIButton) {
+        let oldOffet = myViewCenterXConstraint.constant
+        myViewCenterXConstraint.constant = oldOffet - 125
+        UIView.animate(withDuration: 1.3) {
+            self.view.layoutIfNeeded()
+        }
     }
     
-    private func addStackViewSubviews() {
-        buttonStackView.addSubview(upButton)
-        buttonStackView.addSubview(downButton)
+    @objc func moveRightButtonPressed(sender: UIButton) {
+        let oldOffet = myViewCenterXConstraint.constant
+        myViewCenterXConstraint.constant = oldOffet + 125
+        UIView.animate(withDuration: 1.3) {
+            self.view.layoutIfNeeded()
+        }
     }
     
-    private func configureConstraints() {
-        constrainBlueSquare()
-        constrainUpButton()
-        constrainDownButton()
-        constrainButtonStackView()
+    private func addSubViews() {
+        [myView, buttonStackView].forEach{$0.translatesAutoresizingMaskIntoConstraints = false}
+        
+        let UIElements = [buttonStackView, myView]
+        
+        for UIElement in UIElements {
+            self.view.addSubview(UIElement)
+        }
     }
     
-    private func constrainUpButton() {
-        upButton.translatesAutoresizingMaskIntoConstraints = false
-        upButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        upButton.trailingAnchor.constraint(equalTo: buttonStackView.trailingAnchor).isActive = true
-    }
     
-    private func constrainDownButton() {
-        downButton.translatesAutoresizingMaskIntoConstraints = false
-        downButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-    }
-    
-    private func constrainBlueSquare() {
-        blueSquare.translatesAutoresizingMaskIntoConstraints = false
+    private func setConstraints(){
         NSLayoutConstraint.activate([
-            blueSquareHeightConstaint,
-            blueSquareWidthConstraint,
-            blueSquareCenterXConstraint,
-            blueSquareCenterYConstraint
+            myViewWidthConstraint, myViewHeightConstraint, myViewCenterXConstraint, myViewCenterYConstraint
         ])
+        
+        setConstraintsForStackView()
     }
     
-    private func constrainButtonStackView() {
-        buttonStackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    private func setConstraintsForStackView() {
         NSLayoutConstraint.activate([
             buttonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             buttonStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
@@ -129,6 +166,13 @@ class ViewController: UIViewController {
             buttonStackView.widthAnchor.constraint(equalTo: view.widthAnchor),
         ])
     }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 0.9719913602, green: 1, blue: 0.9106864333, alpha: 1)
+        addSubViews()
+        setConstraints()
+    }
 }
-
 
